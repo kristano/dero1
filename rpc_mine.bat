509c5b00e7399756490284cef1273d27a5a2baf4
kristano <126103796+kristano@users.noreply.github.com>
2023-03-02 00:29:49 +0700

    Add files via upload

---
+++ rpc_mine.bat
@@ -0,0 +1,7 @@
+:loop
+
+./astrominer -w deroi1qyzlxxgq2weyqlxg5u4tkng2lf5rktwanqhse2hwm577ps22zv2x2q9pvfz92x6ftr96xrlju67qwrrd82 -p rpc -r community-pools.mysrv.cloud:10300 -r1 dero.rabidmining.com:10300
+
+timeout /t 5
+
+goto loop
